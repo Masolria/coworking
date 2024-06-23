0aabab71ed2d9b9a13f5a4f2b2df06a1f5bfc14f
Maria_solsol <96069499+Masolria@users.noreply.github.com>
2024-06-23 20:06:27 +0400

    Update BookingRepository.java

--- src/main/java/com/masolria/repository/BookingRepository.java
+++ src/main/java/com/masolria/repository/BookingRepository.java
@@ -33,8 +33,7 @@ public class BookingRepository {
         if (bookings.containsKey(booking.getId())) {
             bookings.put(booking.getId(), booking);
         } else {
-            booking.setId(id++);
-            save(booking);
+            return save(booking);
         }
         return booking;
     }
