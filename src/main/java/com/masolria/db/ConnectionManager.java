b7b8c7e822cd88a24a3bec326e3512a156be5eef
Maria_solsol <mioro.sol@gmail.com>
2024-06-29 20:44:21 +0400

    updated connection manager

--- src/main/java/com/masolria/db/ConnectionManager.java
+++ src/main/java/com/masolria/db/ConnectionManager.java
@@ -7,6 +7,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
+    public Connection getConnection(String url,String user,String password) throws SQLException {
+        return DriverManager.getConnection(url,user,password);
+    }
     public Connection getConnection() throws SQLException {
         PropertiesUtil propertiesUtil = new PropertiesUtil();
         String URL = propertiesUtil.getProperty("postgres.url");
