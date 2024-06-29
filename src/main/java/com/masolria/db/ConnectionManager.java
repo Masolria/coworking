bc944094be7f1922d32db4b5792458aea088b5b4
Maria_solsol <mioro.sol@gmail.com>
2024-06-29 21:38:31 +0400

    deleted properties util logic

--- src/main/java/com/masolria/db/ConnectionManager.java
+++ src/main/java/com/masolria/db/ConnectionManager.java
@@ -1,20 +1,19 @@
 package com.masolria.db;
 
-import com.masolria.util.PropertiesUtil;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    public Connection getConnection(String url,String user,String password) throws SQLException {
-        return DriverManager.getConnection(url,user,password);
+    String URL;
+    String USER;
+    String PASSWORD;
+    public ConnectionManager(String url,String user,String password){
+        URL = url;
+        USER = user;
+        PASSWORD = password;
     }
     public Connection getConnection() throws SQLException {
-        PropertiesUtil propertiesUtil = new PropertiesUtil();
-        String URL = propertiesUtil.getProperty("postgres.url");
-        String USER = propertiesUtil.getProperty("postgres.user");
-        String PASSWORD = propertiesUtil.getProperty("postgres.password");
         return DriverManager.getConnection(URL,
                 USER,
                 PASSWORD);
