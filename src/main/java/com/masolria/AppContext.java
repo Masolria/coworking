a554c3ebd996c36dccca23a227eaffc157f3abae
Maria_solsol <mioro.sol@gmail.com>
2024-06-29 17:57:16 +0400

    updated appcontext

--- src/main/java/com/masolria/AppContext.java
+++ src/main/java/com/masolria/AppContext.java
@@ -4,13 +4,15 @@ import com.masolria.controller.ConsoleController;
 import com.masolria.entity.User;
 import com.masolria.in.Input;
 import com.masolria.out.Output;
-import com.masolria.repository.BookingRepository;
-import com.masolria.repository.SpaceRepository;
-import com.masolria.repository.UserRepository;
+import com.masolria.repository.Jdbc.JdbcBookingRepository;
+import com.masolria.repository.Jdbc.JdbcSpaceRepository;
+import com.masolria.repository.Jdbc.JdbcUserRepository;
 import com.masolria.service.BookingService;
 import com.masolria.service.EntryService;
 import com.masolria.service.SpaceService;
 import com.masolria.service.UserService;
+import com.masolria.db.ConnectionManager;
+import com.masolria.db.LiquibaseRunner;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,14 +32,20 @@ public class AppContext {
     }
 
     public static void loadForInjection() {
-        UserService userService = new UserService(new UserRepository());
-        SpaceService spaceService = new SpaceService(new SpaceRepository());
+        ConnectionManager cManager = new ConnectionManager();
+        CONTEXT.put("connectionManager",cManager);
+        LiquibaseRunner liquibaseRunner = new LiquibaseRunner(cManager);
+        liquibaseRunner.runMigration();
+
+        UserService userService = new UserService(new JdbcUserRepository(cManager));
+        SpaceService spaceService = new SpaceService(new JdbcSpaceRepository(cManager));
         ConsoleController controller = new ConsoleController(userService,
                 spaceService,
                 new EntryService(userService),
-                new BookingService(new BookingRepository(), spaceService));
+                new BookingService(new JdbcBookingRepository(cManager), spaceService));
         CONTEXT.put("consoleController", controller);
         CONTEXT.put("input", new Input());
         CONTEXT.put("output", new Output());
+
     }
 }
