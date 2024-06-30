ea7503aa31c96b451fc6ae76fd2e59b0b4fbab11
Maria_solsol <mioro.sol@gmail.com>
2024-06-30 13:06:34 +0400

    add javadoc

--- src/main/java/com/masolria/util/DateTimeParseUtil.java
+++ src/main/java/com/masolria/util/DateTimeParseUtil.java
@@ -3,17 +3,41 @@ package com.masolria.util;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+/**
+ * The Date time parse util.
+ * Parses LocalDateTime and LocalDate objects from given format String.
+ */
 public class DateTimeParseUtil {
+    /**
+     * The Format local date time.
+     */
     static final String FORMAT_LOCAL_DATE_TIME = "yyyy MM dd HH:mm";
+    /**
+     * The Format local date.
+     */
     static final String FORMAT_LOCAL_DATE = "yyyy MM dd";
+    /**
+     * The LocalDate formatter.
+     */
     static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(FORMAT_LOCAL_DATE);
+    /**
+     * The LocalDateTime formatter.
+     */
     static final DateTimeFormatter dateTimeformatter = DateTimeFormatter.ofPattern(FORMAT_LOCAL_DATE_TIME);
-
+    /**
+     * Parses to LocalDateTime from the given string, which must match the format.     *
+     * @param pattern the pattern with given date and time.
+     * @return the LocalDateTime with given date and time.
+     */
     public static LocalDateTime parseToLDateTime(String pattern) {
         return LocalDateTime.from(dateTimeformatter.parse(pattern));
     }
-
+    /**
+     * Parse to localDate from the given string, which must match the format.
+     *
+     * @param pattern the pattern with given date.
+     * @return the LocalDate object with given date.
+     */
     public static LocalDate parseToLDate(String pattern) {
         return LocalDate.from(dateFormatter.parse(pattern));
     }
--- src/main/java/com/masolria/util/PropertiesUtil.java
+++ src/main/java/com/masolria/util/PropertiesUtil.java
@@ -3,13 +3,26 @@ package com.masolria.util;
 import java.io.*;
 import java.util.Properties;
 
+/**
+ * The Properties util class.
+ * Loads properties from resources and gives properties values.
+ */
 public class PropertiesUtil {
+    /**
+     * the object with properties
+     */
     private final Properties properties = new Properties();
 
+    /**
+     * Instantiates a new Properties util.
+     */
     public PropertiesUtil() {
         this.loadProperties();
     }
 
+    /**
+     * Load properties from application.properties to the properties field.
+     */
     public void loadProperties() {
 
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
@@ -21,6 +34,12 @@ public class PropertiesUtil {
         }
     }
 
+    /**
+     * Gets property value by given key.
+     *
+     * @param key the String key of properties entry.
+     * @return the property value
+     */
     public String getProperty(String key) {
         return properties.getProperty(key);
     }
