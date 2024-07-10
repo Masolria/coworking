2a24bb9c8f0f271e684ea4e73aecba2da67e97a8
Maria_solsol <mioro.sol@gmail.com>
2024-07-10 14:22:57 +0400

    Revert "deleted unused files"
    This reverts commit d1dd149f2091863d42efe235c5a4489dfb86dd56.

---
+++ src/main/java/com/masolria/handleView/HandleBookingView.java
@@ -0,0 +1,178 @@
+package com.masolria.handleView;
+
+import com.masolria.AppContext;
+import com.masolria.controller.ConsoleController;
+import com.masolria.entity.Booking;
+import com.masolria.entity.SpaceType;
+import com.masolria.entity.User;
+import com.masolria.exception.OccupiedConflictException;
+import com.masolria.in.Input;
+import com.masolria.out.Output;
+import com.masolria.util.DateTimeParseUtil;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The Handle booking view class.
+ * Represents methods for console input/output and interaction with console controller.
+ */
+public class HandleBookingView {
+
+    /**
+     * Shows free booking slots.
+     *
+     * @param output     the output
+     * @param controller the controller
+     */
+    public static void showFreeSlots(Output output, ConsoleController controller) {
+        output.output("There are all free slots");
+        List<Booking> bookings = controller.getAllFreeSlots();
+        for (Booking booking : bookings) {
+            output.output(booking.toString());
+        }
+    }
+
+    /**
+     * Shows booking by user email.
+     *
+     * @param input      the input
+     * @param output     the output
+     * @param controller the controller
+     */
+    public static void showByUser(Input input, Output output, ConsoleController controller) {
+        output.output("Write the email of user whose bookings you would like to see");
+        List<Booking> bookings = controller.getAllBookingByUserEmail(input.input());
+        if (bookings.isEmpty()) {
+            output.output("There are no such user with given email");
+        } else {
+            output.output("There are all booking by given user");
+            for (Booking booking : bookings) {
+                output.output(booking.toString());
+            }
+        }
+    }
+
+    /**
+     * Shows bookings by type.
+     *
+     * @param input      the input
+     * @param output     the output
+     * @param controller the controller
+     */
+    public static void ShowByType(Input input, Output output, ConsoleController controller) {
+        output.output("""
+                Write the type of booking space you would like to see:
+                1.working space
+                2.conference hall
+                """);
+        Long spaceNum = Long.valueOf(input.input());
+        SpaceType spaceType;
+        if (spaceNum.equals(1L)) {
+            spaceType = SpaceType.WORKING_SPACE;
+        } else if (spaceNum.equals(2L)) {
+            spaceType = SpaceType.CONFERENCE_HALL;
+        } else {
+            output.output("Your space type input is not correct");
+            return;
+        }
+
+        List<Booking> bookings = controller.getAllBookingByType(spaceType);
+        output.output("There are all booking by given type");
+        for (Booking booking : bookings) {
+            output.output(booking.toString());
+        }
+    }
+
+    /**
+     * Shows bookings by given date.
+     *
+     * @param input      the input
+     * @param output     the output
+     * @param controller the controller
+     */
+    public static void ShowByDate(Input input, Output output, ConsoleController controller) {
+        output.output("Write the date you what to see bookings, in the format yyyy mm dd");
+        String format = input.input();
+        try {
+            LocalDate localDate = DateTimeParseUtil.parseToLDate(format);
+            List<Booking> bookings = controller.getBookingByDate(localDate);
+            output.output("There are all booking by given date");
+            for (Booking booking : bookings) {
+                output.output(booking.toString());
+            }
+        } catch (DateTimeException e) {
+            System.out.println("Your date input is not correct");
+        }
+    }
+
+    /**
+     * Provides interaction  to controller through input and output.
+     * Performs reservation a booking slot by its id.
+     *
+     * @param input      the input
+     * @param output     the output
+     * @param controller the controller
+     */
+    public static void showReserve(Input input, Output output, ConsoleController controller) {
+        output.output("Write the id of the booking entry you want to keep for yourself");
+        showFreeSlots(output, controller);
+        try {
+            Long bookingId = Long.parseLong(input.input());
+            Optional<Booking> optionalBooking = controller.getBookingById(bookingId);
+            if (optionalBooking.isPresent()) {
+                Booking booking = optionalBooking.get();
+                if (booking.isBooked()) {
+                    throw new OccupiedConflictException("Sorry, this slot is already booked");
+                }
+                booking.setBooked(true);
+                booking.setForUserId(AppContext.getAuthorizedUser().getId());
+                controller.updateBooking(booking);
+                output.output("You have successfully booked a space");
+                return;
+            }
+            output.output("Sorry, slot with given id doesn't exist");
+        } catch (NumberFormatException e) {
+            output.output("Your input isn't correct");
+        }
+    }
+
+    /**
+     * Performs releasing booking slot.
+     *
+     * @param input      the input
+     * @param output     the output
+     * @param controller the controller
+     */
+    public static void showReleaseBooking(Input input, Output output, ConsoleController controller) {
+        User user = AppContext.getAuthorizedUser();
+        List<Booking> bookings = controller.getAllBookingByUser(user);
+        output.output("There are all your bookings, write id you want to release");
+        for (Booking booking : bookings) {
+            output.output(booking.toString());
+        }
+        try {
+            Long id = Long.parseLong(input.input());
+            Optional<Booking> bookingOptional = controller.getBookingById(id);
+            if (bookingOptional.isPresent()) {
+                Booking booking = bookingOptional.get();
+                if (booking.getForUserId().equals(user.getId())) {
+                    booking.setBooked(false);
+                    booking.setForUserId(null);
+                    controller.updateBooking(booking);
+                    output.output("Booking released successfully.");
+                } else {
+                    output.output("You can only release your own bookings");
+                }
+            } else {
+                output.output("There is no such booking with the given id");
+            }
+        } catch (NumberFormatException e) {
+            output.output("Your input isn't correct");
+        } catch (RuntimeException e) {
+            output.output("Unrecognized exception here");
+        }
+    }
+}
