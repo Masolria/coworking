e9411752b12eb77af585fecc2e5015c38dc1401a
Maria_solsol <mioro.sol@gmail.com>
2024-07-10 17:30:32 +0400

    update boolean

--- src/main/java/com/masolria/Mapper/BookingMapper.java
+++ src/main/java/com/masolria/Mapper/BookingMapper.java
@@ -4,11 +4,13 @@ import com.masolria.dto.BookingDto;
 import com.masolria.entity.Booking;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface BookingMapper {
+    @Mapping(source = "booked", target = "isBooked")
     BookingDto toDto(Booking booking);
     Booking toEntity(BookingDto bookingDto);
     List<BookingDto> toDtoList(List<Booking> bookings);
